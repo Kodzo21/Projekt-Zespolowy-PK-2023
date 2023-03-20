de2cedd87a2138e8c604f9da85996ef543287c9a
Lukasz <ryczekl21@gmail.com>
2023-03-20 16:35:05 +0100

    initial commit

---
+++ backend/src/main/java/com/example/gigachatb/GigachatBApplication.java
@@ -0,0 +1,13 @@
+package com.example.gigachatb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigachatBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GigachatBApplication.class, args);
+    }
+
+}
