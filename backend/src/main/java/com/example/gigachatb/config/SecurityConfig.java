3a9ade7fe771b0b70997a7a4bf8388bfb4229565
Lukasz <ryczekl21@gmail.com>
2023-06-06 14:45:28 +0200

    security changes

--- backend/src/main/java/com/example/gigachatb/config/SecurityConfig.java
+++ backend/src/main/java/com/example/gigachatb/config/SecurityConfig.java
@@ -5,11 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,10 +26,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors()
+                .and()
                 .csrf()
                 .disable()
+                .headers(headers-> headers.frameOptions().disable())
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/api/v1/auth/**","/ws","/ws/**","/topic/**","/app/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -37,4 +44,19 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+            var cors = new CorsConfiguration();
+            cors.setAllowCredentials(true);
+            cors.addAllowedOrigin("http://localhost:4200");
+            cors.addAllowedHeader("*");
+            cors.addAllowedMethod("*");
+            var source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**",cors);
+            return source;
+    }
+
+
+
 }
